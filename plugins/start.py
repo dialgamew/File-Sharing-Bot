223ac505b330053bc28423d0c1ebefd4f3dd16eb
dialgamew <77679123+dialgamew@users.noreply.github.com>
2021-11-25 18:33:16 +0600

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -109,6 +109,29 @@ async def start_command(client: Client, message: Message):
         )
         return
 
+@Bot.on_message(filters.command('start') & filters.private & subscribed)
+async def not_joined(client: Client, message: Message):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                 "Authorization Info",
+                url = f"https://t.me/fateverse"
+            )
+        ]
+    ]
+    await message.reply(
+        text = AUTH_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+    )
+  
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     buttons = [
